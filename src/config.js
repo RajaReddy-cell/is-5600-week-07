b8791fee4b99593945439fc2a22ffc448474ae85
RajaReddy-cell <raja.reddy@slu.edu>
2025-03-05 04:48:49 +0000

    week7

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://orange-train-695wrw5rjpvp34vrx-3080.app.github.dev";
